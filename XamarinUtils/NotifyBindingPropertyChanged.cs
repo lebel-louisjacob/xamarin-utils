1d121b080df24abf555075885381d3d3b5a73a24
Louis-Jacob Lebel <lebel.louisjacob@gmail.com>
2020-09-24 00:59:34 -0400

    first commit

---
+++ XamarinUtils/NotifyBindingPropertyChanged.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace XamarinUtils
+{
+    public abstract class NotifyBindingPropertyChanged : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void UpdateBinding<A>(ref A currentValue, in A newValue, [CallerMemberName] in string propertyName = "")
+        {
+            if (!newValue.Equals(currentValue))
+            {
+                currentValue = newValue;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ XamarinUtils/TypedValueConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace XamarinUtils
+{
+    public abstract class TypedValueConverter<To, From> : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return TypedConvert((From) (value ?? default(From)));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return TypedConvertBack((To) value);
+        }
+
+        public abstract To TypedConvert(in From input);
+
+        public From TypedConvertBack(in To input)
+        {
+            throw new NotSupportedException($"{GetType().Name} cannot be used on two-way bindings.");
+        }
+    }
+}
